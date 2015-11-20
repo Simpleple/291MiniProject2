8d6cae24394416bfb9b8f6aac212b31c6963b7a1
Simpleple <zhao2@cs.ualberta.ca>
2015-11-19 23:02:31 -0700

    pt search

--- dbSearch.py
+++ dbSearch.py
@@ -1,12 +1,18 @@
 from bsddb3 import db
+from array import array
 
-database = db.DB()
-database.open("sc.idx")
-cur = database.cursor()
-iter = cur.first()
-while iter:
-    print(iter)
-    iter = cur.next()
-cur.close()
-print(database.get(b'3.0'))
-database.close()
+def searchPterms(key):
+    key = key.encode('utf-8')
+    database = db.DB()
+    database.open("pt.idx")
+    cur = database.cursor()
+    a = array("i")
+    data = int(cur.get(key, db.DB_SET)[1].decode('utf-8'))
+    a.append(data)
+    while cur.next_dup():
+        data = int(cur.get(db.DB_CURRENT)[1].decode('utf-8'))
+        a.append(data)
+    database.close()
+    return sorted(a.tolist())
+
+print(searchPterms("soundtrack"))
