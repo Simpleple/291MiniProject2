dc996cee9455ccd956639687c57b3fe967c2ae5e
Simpleple <zhao2@cs.ualberta.ca>
2015-11-20 12:44:54 -0700

    various search

--- dbSearch.py
+++ dbSearch.py
@@ -1,51 +1,58 @@
 from bsddb3 import db
-from array import array
 
 def exactSearch(key, filename):
     key = key.encode('utf-8')
     database = db.DB()
     database.open(filename)
     cur = database.cursor()
-    a = array("i")
+    resultSet = set()
     data = int(cur.get(key, db.DB_SET)[1].decode('utf-8'))
-    a.append(data)
+    resultSet.add(data)
     while cur.next_dup():
         data = int(cur.get(db.DB_CURRENT)[1].decode('utf-8'))
-        a.append(data)
+        resultSet.add(data)
     database.close()
-    return sorted(a.tolist())
+    return resultSet
 
 def partialSearch(key, filename):
     encodedkey = key.encode('utf-8')
     database = db.DB()
     database.open(filename)
     cur = database.cursor()
-    a = array("i")
+    resultSet = set()
     data = int(cur.get(encodedkey, db.DB_SET_RANGE)[1].decode('utf-8'))
-    a.append(data)
+    resultSet.add(data)
     while str(cur.next()[0].decode('utf-8')).startswith(key):
         data = int(cur.get(db.DB_CURRENT)[1].decode('utf-8'))
-        a.append(data)
+        resultSet.add(data)
     database.close()
-    return sorted(a.tolist())
+    return resultSet
 
 def rangeSearch(key, filename, compare):
     key = key.encode('utf-8')
     database = db.DB()
     database.open(filename)
     cur = database.cursor()
-    a = array("i")
+    resultSet = set()
     data = int(cur.get(key, db.DB_SET_RANGE)[1].decode('utf-8'))
     if compare == "bigger":
-        a.append(data)
+        resultSet.add(data)
         while cur.next():
             data = int(cur.get(db.DB_CURRENT)[1].decode('utf-8'))
-            a.append(data)
+            resultSet.add(data)
     elif compare == "less":
         while cur.prev():
             data = int(cur.get(db.DB_CURRENT)[1].decode('utf-8'))
-            a.append(data)
+            resultSet.add(data)
     database.close()
-    return sorted(a.tolist())
+    return resultSet
     
-print(rangeSearch("4.9", "sc.idx", "less"))
+def printFullRecord(recno):
+    database = db.DB()
+    database.open("rw.idx")
+    print(database.get(recno.encode('utf-8')))
+    database.close()
+
+#print(rangeSearch("4.9", "sc.idx", "less"))
+#print(exactSearch("soundtrack", "pt.idx"))
+printFullRecord("1")
