944194dd6348c00ec107586cc81fa469fd406a07
Simpleple <zhao2@cs.ualberta.ca>
2015-11-20 11:54:09 -0700

    various search

--- dbSearch.py
+++ dbSearch.py
@@ -1,10 +1,10 @@
 from bsddb3 import db
 from array import array
 
-def searchPterms(key):
+def exactSearch(key, filename):
     key = key.encode('utf-8')
     database = db.DB()
-    database.open("pt.idx")
+    database.open(filename)
     cur = database.cursor()
     a = array("i")
     data = int(cur.get(key, db.DB_SET)[1].decode('utf-8'))
@@ -15,4 +15,37 @@ def searchPterms(key):
     database.close()
     return sorted(a.tolist())
 
-print(searchPterms("soundtrack"))
+def partialSearch(key, filename):
+    encodedkey = key.encode('utf-8')
+    database = db.DB()
+    database.open(filename)
+    cur = database.cursor()
+    a = array("i")
+    data = int(cur.get(encodedkey, db.DB_SET_RANGE)[1].decode('utf-8'))
+    a.append(data)
+    while str(cur.next()[0].decode('utf-8')).startswith(key):
+        data = int(cur.get(db.DB_CURRENT)[1].decode('utf-8'))
+        a.append(data)
+    database.close()
+    return sorted(a.tolist())
+
+def rangeSearch(key, filename, compare):
+    key = key.encode('utf-8')
+    database = db.DB()
+    database.open(filename)
+    cur = database.cursor()
+    a = array("i")
+    data = int(cur.get(key, db.DB_SET_RANGE)[1].decode('utf-8'))
+    if compare == "bigger":
+        a.append(data)
+        while cur.next():
+            data = int(cur.get(db.DB_CURRENT)[1].decode('utf-8'))
+            a.append(data)
+    elif compare == "less":
+        while cur.prev():
+            data = int(cur.get(db.DB_CURRENT)[1].decode('utf-8'))
+            a.append(data)
+    database.close()
+    return sorted(a.tolist())
+    
+print(rangeSearch("4.9", "sc.idx", "less"))
